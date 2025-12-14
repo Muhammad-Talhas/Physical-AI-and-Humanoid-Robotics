9be0c37e61371ebe6d02093ade56584ae9743257
Muhammad Talha <164399094+Muhammad-Talhas@users.noreply.github.com>
2025-12-15 01:57:13 +0500

    commit

--- Frontend/docusaurus.config.ts
+++ Frontend/docusaurus.config.ts
@@ -10,7 +10,13 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://physical-ai-humanoid-robotics.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Muhammad-Talhas', // Usually your GitHub org/user name.
+  projectName: 'Physical-AI-and-Humanoid-Robotics', // Usually your repo name.
 
   onBrokenLinks: 'warn',
 
